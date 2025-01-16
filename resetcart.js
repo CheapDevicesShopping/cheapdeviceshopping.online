19290f5d4c90432d572484dfc4ec9fcd3ab2a2a9
markdev <markpavlenkos@gmail.com>
2025-01-16 15:46:21 +0200

    edit

--- resetcart.js
+++ resetcart.js
@@ -3,12 +3,12 @@ const clearCartButton = document.getElementById('clear-cart')
 
 //clear cart functionality
 clearCartButton.addEventListener('click', () => {
-    const confirmClear = confirm('Are sure bro');
+    const confirmClear = confirm('are you sure?');
     if (confirmClear) {
       //clear local storage and cart display
       localStorage.removeItem('cart');
       cartItemsDiv.innerHTML = ''; //Clear items in the cart display
       totalPriceElement.textContent = '0.00'; //Reset total price
-      alert('Cart has been cleared :')
+      alert('cleared')
     }
   })
